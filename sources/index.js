2d373e3ff8d02915c38f1932a2895f0f6c47571b
egpennington <egpennington@hotmail.com>
2024-01-17 16:20:03 -0600

    fixed js bug - have to use  index.js in sources

--- sources/index.js
+++ sources/index.js
@@ -1,12 +1,26 @@
 console.log("hello worldly peeps")
 
 document.body.innerHTML= `
-    <h1>Hello</h1>
-    <h2>here i am here</h2>
-    <p>Day2Day Tracking App</p>
-    <ul>
-        <li>Monthly</li>
-        <li>Daily</li>
-        <li>Today</li>
-    </ul>        
+<header>
+<h1>Time Harbor - Day2Day</h1>
+</header>
+
+<nav>
+<ul>
+    <li><a href="#">Home</a></li>
+    <li><a href="#">Tasks</a></li>
+    <li><a href="#">Calendar</a></li>
+</ul>
+</nav>
+
+<div class="container">
+<main>
+    <!-- <h1>Hello</h1>
+    <h2>here i am here</h2> -->
+</main>
+</div>
+
+<footer>
+&copy; 2024 Time Harbor - Day2Day
+</footer>     
     `
