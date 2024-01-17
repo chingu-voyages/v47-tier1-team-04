b9fa37486868a507f3ecf271ceb67f8383462dbc
egpennington <egpennington@hotmail.com>
2024-01-17 16:27:12 -0600

    added static daily activities

--- sources/index.js
+++ sources/index.js
@@ -14,10 +14,37 @@ document.body.innerHTML= `
 </nav>
 
 <div class="container">
-<main>
-    <!-- <h1>Hello</h1>
-    <h2>here i am here</h2> -->
-</main>
+    <main>
+        <section class="daily-activities">
+            <h2>Daily Activities</h2>
+                <ul>
+                <!-- Activity 1 -->
+                    <li>
+                        <strong>Activity:</strong> Work on Project
+                        <br>
+                        <strong>Time:</strong> 10:00 AM - 12:00 PM
+                        <br>
+                        <strong>Place:</strong> Home Office
+                    </li>
+                    <!-- Activity 2 -->
+                    <li>
+                        <strong>Activity:</strong> Exercise
+                        <br>
+                        <strong>Time:</strong> 5:00 PM - 6:00 PM
+                        <br>
+                        <strong>Place:</strong> Gym
+                    </li>
+                    <!-- Activity 3 -->
+                    <li>
+                        <strong>Activity:</strong> Read a Book
+                        <br>
+                        <strong>Time:</strong> 8:00 PM - 9:00 PM
+                        <br>
+                        <strong>Place:</strong> Living Room
+                    </li>
+                </ul>
+        </section>
+    </main>
 </div>
 
 <footer>
