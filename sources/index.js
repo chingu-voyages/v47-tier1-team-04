c9533bf9087339eb49f4085c79782706c0378a2a
egpennington <egpennington@hotmail.com>
2024-01-17 15:43:15 -0600

    added ul and li for tracking

--- sources/index.js
+++ sources/index.js
@@ -4,4 +4,9 @@ document.body.innerHTML= `
     <h1>Hello</h1>
     <h2>here i am here</h2>
     <p>Day2Day Tracking App</p>
+    <ul>
+        <li>Monthly</li>
+        <li>Daily</li>
+        <li>Today</li>
+    </ul>        
     `
