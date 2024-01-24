1575cb655b69c8b7aa2a5632d2cb0701eabc6c43
egpennington <egpennington@hotmail.com>
2024-01-24 13:06:05 -0600

    create data.js for userActivity data, create export and import in js, add console.log for test

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,15 @@
+import { userActivities } from "./data.js"
+
 console.log("In the harbor, boats are safe; but they were built for voyages.")
+
+for (let category of userActivities) {
+    console.log("Category:", category.categoryName);
+  
+    for (let activityType of category.activityTypes) {
+      console.log("  Activity Type:", activityType.activityName);
+  
+      for (let task of activityType.Tasks) {
+        console.log("    Task Name:", task.taskName);
+      }
+    }
+  }
