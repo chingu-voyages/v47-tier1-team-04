e8c8bd33f6aedb29fb60c237c1daed082051c1cb
Amanda_L <amandalibbylab@gmail.com>
2024-02-01 20:43:27 -0500

    dynamically generated html on page

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,6 @@
-(function () {
+import Controller from "./controller.js";
     
-    class View { // What the app looks like, what the user can see and do, User Interface
+   export class View { // What the app looks like, what the user can see and do, User Interface
         constructor(element, content, anchor, id, classList) {
             const container = document.createElement(element);
           container.innerHTML = content;
@@ -18,8 +18,8 @@
           anchor.append(container);
           this.container = container;
         }
-        init() {
-            this.renderAside("My Daily Checklist");
+        init(title) {
+            this.renderAsideGroups(title);
             this.renderNavBar();
             this.renderContent();
             this.renderFooter();
@@ -39,40 +39,32 @@
         </div>            
 
         <h2>${title}</h2>
-            <div class="daily-checklist">
-                <div class="activity">
-                    <h3 id="category-title-1">Routine Activities <i class="fa-solid fa-circle-chevron-down"></i></h3>
-                    <ul id="activity-el">
-                        <li id="task-1-1">Projects</li>
-                        <li id="task-1-2">Blog Posts</li>
-                    </ul>
-                </div>
+            <div id="daily-checklist">
 
-                <div class="activity">
-                    <h3 id="category-title-2">Studying <i class="fa-solid fa-circle-chevron-down"></i></h3>
-                    <ul>
-                        <li id="task-2-1">Mongo DB</li>
-                        <li id="task-2-2">Blog Posts</li> 
-                    </ul>
-                </div>
-                
-                <div class="activity">
-                    <h3 id="category-title-3">Daily Task Project <i class="fa-solid fa-circle-chevron-down"></i></h3>
-                    <ul>
-                        <li id="task-3-1">Backlog</li>
-                        <li id="task-3-2">Coding</li>
-                    </ul>
-                </div>
-
-                <div class="activity">
-                    <h3 id="category-title-4">Chingu <i class="fa-solid fa-circle-chevron-down"></i></h3>
-                    <ul>
-                        <li id="task-4-1">Voyage</li>
-                        <li id="task-4-2">Pair Programming</li>
-                        <li id="task-4-3">Physical Exercise</li>
-                    </ul>
-                </div>
             </div>`, document.getElementById("app"), "element-el", "aside")
+        }
+        renderAsideGroup(group) {
+          console.log(group)
+          this.createElement("div", `
+            
+              <h3 id="category-title-1">${group} <i class="fa-solid fa-circle-chevron-down"></i></h3>
+              <ul id="sidebar_${group}">
+                  
+              </ul>
+            `, document.getElementById("daily-checklist", null, "activity")
+          )
+        }
+        renderAsideGroups(title) {
+          console.log(app.controller.returnUniqueGroupNames())
+          let aside = this.renderAside(title);
+          app.controller.returnUniqueGroupNames().map(group => {
+            this.renderAsideGroup(group);
+            app.controller.returnUniqueCategoriesByGroup(group).map(category => this.renderAsideCatagory(category, group));
+          })
+            
+        }
+        renderAsideCatagory(category, group) {
+          app.view.createElement("li", category, document.getElementById(`sidebar_${group}`))
         }
 
         renderNavBar() {
@@ -178,11 +170,12 @@
         this.tasks = [];
         this.views = [];
         this.view = new View("div", " <!--Code injected by Amanda-->", document.body, "app", "container")
+        this.controller = new Controller();
       }
       //Function to initialize app:
-      init() {
-        this.seed();
-        this.view.init();
+      async init(title) {
+        await this.seed();
+        this.controller.init(title);
         return this;
       }
       //Function to clear/reset tasks:
@@ -210,9 +203,9 @@
       }
     }
     const app = new App();
-  
+    export default app
     //Object constructor to create new tasks:
-    class Task {
+   export class Task {
       constructor(name, group, category, frequency, days, calander) {
         this.name = name;
         this.group = group;
@@ -246,7 +239,7 @@
 
    
   
-    app.init();
+    app.init("My Daily Classlist");
     // setTimeout(() => console.log(app), 50)
     // setTimeout(() => console.log(app.tasks[0].read()), 50)
     //setTimeout(() => console.log(app.tasks[0].update('new','change','from','method','that')), 50)
@@ -255,4 +248,3 @@
   const groups = () => [...new Set(app.tasks.map(task => task.group))]
    // setTimeout(() => groups().map(group => new View(group)), 50);
     console.log(app)
-  })();
