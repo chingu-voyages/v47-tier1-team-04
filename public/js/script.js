eaf6a738c370fcc1d38341969c4a633344bbe4fc
Mikey Nichols <mnix@journeytocode.io>
2024-02-03 18:48:50 -0500

    reformats document

--- public/js/script.js
+++ public/js/script.js
@@ -1,35 +1,38 @@
 import Controller from "./controller.js";
-    
-   export class View { // What the app looks like, what the user can see and do, User Interface
-        constructor(element, content, anchor, id, classList) {
-            const container = document.createElement(element);
-          container.innerHTML = content;
-          if (id) {
-              container.id = id;
-              this.id = id;
-          } else {
-              container.id = `view_${viewIndex}`;
-              this.id = container.id;
-              viewIndex++;
-          }
-          if (classList) {
-              container.classList = classList;
-          }
-          anchor.append(container);
-          this.container = container;
-        }
-        init(title) {
-            this.renderAsideGroups(title);
-            this.renderNavBar();
-            this.renderContent();
-            this.renderFooter();
-        }
-        createElement(element, content, anchor, id, classList) {
-            const newView = new View(element, content, anchor, id, classList);
-            app.views.push(newView);
-        }
-        renderAside(title) {
-            return this.createElement("aside", ` <div class="avatar-area">
+
+export class View {
+  // What the app looks like, what the user can see and do, User Interface
+  constructor(element, content, anchor, id, classList) {
+    const container = document.createElement(element);
+    container.innerHTML = content;
+    if (id) {
+      container.id = id;
+      this.id = id;
+    } else {
+      container.id = `view_${viewIndex}`;
+      this.id = container.id;
+      viewIndex++;
+    }
+    if (classList) {
+      container.classList = classList;
+    }
+    anchor.append(container);
+    this.container = container;
+  }
+  init(title) {
+    this.renderAsideGroups(title);
+    this.renderNavBar();
+    this.renderContent();
+    this.renderFooter();
+  }
+  createElement(element, content, anchor, id, classList) {
+    const newView = new View(element, content, anchor, id, classList);
+    app.views.push(newView);
+  }
+  renderAside(title) {
+    return this.createElement(
+      "aside",
+      ` <div class="avatar-area">
             <div class="avatar">
                 <img src="./img/Friendly Ones Avatar and Backdrop.png" alt="avatar pict">
             </div>
@@ -41,36 +44,49 @@ import Controller from "./controller.js";
         <h2>${title}</h2>
             <div id="daily-checklist">
 
-            </div>`, document.getElementById("app"), "element-el", "aside")
-        }
-        renderAsideGroup(group) {
-          console.log(group)
-          this.createElement("div", `
+            </div>`,
+      document.getElementById("app"),
+      "element-el",
+      "aside"
+    );
+  }
+  renderAsideGroup(group) {
+    console.log(group);
+    this.createElement(
+      "div",
+      `
             
               <h3 id="category-title-1">${group} <i class="fa-solid fa-circle-chevron-down"></i></h3>
               <ul id="sidebar_${group}">
                   
               </ul>
-            `, document.getElementById("daily-checklist", null, "activity")
-          )
-        }
-        renderAsideGroups(title) {
-          console.log(app.controller.returnUniqueGroupNames())
-          let aside = this.renderAside(title);
-          app.controller.returnUniqueGroupNames().map(group => {
-            this.renderAsideGroup(group);
-            app.controller.returnUniqueCategoriesByGroup(group).map(category => this.renderAsideCatagory(category, group));
-          })
-            
-        }
-        renderAsideCatagory(category, group) {
-          app.view.createElement("li", category, document.getElementById(`sidebar_${group}`))
-        }
+            `,
+      document.getElementById("daily-checklist", null, "activity")
+    );
+  }
+  renderAsideGroups(title) {
+    console.log(app.controller.returnUniqueGroupNames());
+    let aside = this.renderAside(title);
+    app.controller.returnUniqueGroupNames().map((group) => {
+      this.renderAsideGroup(group);
+      app.controller
+        .returnUniqueCategoriesByGroup(group)
+        .map((category) => this.renderAsideCatagory(category, group));
+    });
+  }
+  renderAsideCatagory(category, group) {
+    app.view.createElement(
+      "li",
+      category,
+      document.getElementById(`sidebar_${group}`)
+    );
+  }
 
-        renderNavBar() {
-          return this.createElement( "navbar", 
-          
-          `<nav class="navbar">
+  renderNavBar() {
+    return this.createElement(
+      "navbar",
+
+      `<nav class="navbar">
             <div class="navbar-top">
                 <i class="fa-solid fa-bars menu-btn fa-2x" id="menu-btn"></i>
                 <div id="date" class="date">Today:</div>
@@ -91,11 +107,16 @@ import Controller from "./controller.js";
                 <a href="#" class="btn-month">Month</a>
                 <a href="#" class="btn-year">Year</a>      
             </div>
-          </nav>` , document.getElementById("app"), "element-el", "navbar")
-        }
-        renderContent() {
-          return this.createElement( "content", 
-          `<content class="content">
+          </nav>`,
+      document.getElementById("app"),
+      "element-el",
+      "navbar"
+    );
+  }
+  renderContent() {
+    return this.createElement(
+      "content",
+      `<content class="content">
           <div class="content-search">
               <div class="priority">
                   <a href="#" class="btn btn-lite btn-blue">Low</a>
@@ -144,11 +165,16 @@ import Controller from "./controller.js";
                       </div>
                   </div>
               </div>  
-          </content>`, document.getElementById("app"), "element-el", "content")
-        }
-        renderFooter() {
-          return this.createElement( "footer", 
-          `<footer>
+          </content>`,
+      document.getElementById("app"),
+      "element-el",
+      "content"
+    );
+  }
+  renderFooter() {
+    return this.createElement(
+      "footer",
+      `<footer>
               <div class="footer-left">
                 <a href="https://github.com/chingu-voyages/v47-tier1-team-04">
                   <p class="copyright">© Chingu Team 04 Github</p>
@@ -160,91 +186,98 @@ import Controller from "./controller.js";
                   <img class="chingu-logo" src="./img/chingo-logo.png"
                     />
               </a>            
-            </footer> `, document.getElementById("app"), "element-el", "footer")
-        }
+            </footer> `,
+      document.getElementById("app"),
+      "element-el",
+      "footer"
+    );
+  }
+}
 
-    }
-
-    class App {
-      constructor() {
-        this.tasks = [];
-        this.views = [];
-        this.view = new View("div", " <!--Code injected by Amanda-->", document.body, "app", "container")
-        this.controller = new Controller();
-      }
-      //Function to initialize app:
-      async init(title) {
-        await this.seed();
-        this.controller.init(title);
-        return this;
-      }
-      //Function to clear/reset tasks:
-      resetState() {
-        this.tasks = [];
-        return this;
-      }
-      //Function to pull in the data from the data.model.json file:
-      async seed() {
-        await fetch("./js/data.model.json")
-          .then((res) => res.json())
-          .then((data) =>
-            data.map(
-              (task) =>
-                new Task(
-                  task.name,
-                  task.group,
-                  task.category,
-                  task.frequency,
-                  task.days,
-                  task.calendar
-                )
+class App {
+  constructor() {
+    this.tasks = [];
+    this.views = [];
+    this.view = new View(
+      "div",
+      " <!--Code injected by Amanda-->",
+      document.body,
+      "app",
+      "container"
+    );
+    this.controller = new Controller();
+  }
+  //Function to initialize app:
+  async init(title) {
+    await this.seed();
+    this.controller.init(title);
+    return this;
+  }
+  //Function to clear/reset tasks:
+  resetState() {
+    this.tasks = [];
+    return this;
+  }
+  //Function to pull in the data from the data.model.json file:
+  async seed() {
+    await fetch("./js/data.model.json")
+      .then((res) => res.json())
+      .then((data) =>
+        data.map(
+          (task) =>
+            new Task(
+              task.name,
+              task.group,
+              task.category,
+              task.frequency,
+              task.days,
+              task.calendar
             )
-          );
-      }
-    }
-    const app = new App();
-    export default app
-    //Object constructor to create new tasks:
-   export class Task {
-      constructor(name, group, category, frequency, days, calander) {
-        this.name = name;
-        this.group = group;
-        this.category = category;
-        this.frequency = frequency;
-        this.days = days;
-        this.calander = calander;
-        this.complete = false;
-        app.tasks.push(this);
-      }
+        )
+      );
+  }
+}
+const app = new App();
+export default app;
+//Object constructor to create new tasks:
+export class Task {
+  constructor(name, group, category, frequency, days, calander) {
+    this.name = name;
+    this.group = group;
+    this.category = category;
+    this.frequency = frequency;
+    this.days = days;
+    this.calander = calander;
+    this.complete = false;
+    app.tasks.push(this);
+  }
 
-      read() {
-        this.complete = false;
-        return this;
-      }
-      //Function to create new tasks:
-      update(name, group, category, frequency, days, calander) {
-        this.name = name;
-        this.group = group;
-        this.category = category;
-        this.frequency = frequency;
-        this.days = days;
-        this.calander = calander;
-        this.complete = false;
-        return this;
-      }
-    }
-  
-    //Function to display the data into HTML:
-    let viewIndex = 1;
+  read() {
+    this.complete = false;
+    return this;
+  }
+  //Function to create new tasks:
+  update(name, group, category, frequency, days, calander) {
+    this.name = name;
+    this.group = group;
+    this.category = category;
+    this.frequency = frequency;
+    this.days = days;
+    this.calander = calander;
+    this.complete = false;
+    return this;
+  }
+}
 
-   
-  
-    app.init("My Daily Classlist");
-    // setTimeout(() => console.log(app), 50)
-    // setTimeout(() => console.log(app.tasks[0].read()), 50)
-    //setTimeout(() => console.log(app.tasks[0].update('new','change','from','method','that')), 50)
-    // setTimeout(() => console.log(app.tasks.filter(task => task.group === "STUDYING")), 50)
-  // setTimeout(() =>   console.log([...new Set(app.tasks.map(task => task.group))]), 50)
-  const groups = () => [...new Set(app.tasks.map(task => task.group))]
-   // setTimeout(() => groups().map(group => new View(group)), 50);
-    console.log(app)
+//Function to display the data into HTML:
+let viewIndex = 1;
+
+app.init("My Daily Classlist");
+// setTimeout(() => console.log(app), 50)
+// setTimeout(() => console.log(app.tasks[0].read()), 50)
+//setTimeout(() => console.log(app.tasks[0].update('new','change','from','method','that')), 50)
+// setTimeout(() => console.log(app.tasks.filter(task => task.group === "STUDYING")), 50)
+// setTimeout(() =>   console.log([...new Set(app.tasks.map(task => task.group))]), 50)
+const groups = () => [...new Set(app.tasks.map((task) => task.group))];
+// setTimeout(() => groups().map(group => new View(group)), 50);
+console.log(app);
