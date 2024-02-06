a5778d7656b9dce0b15fca7865d23e372db818c4
Mikey Nichols <mnix@journeytocode.io>
2024-02-06 18:28:36 -0500

    removes comment at end of file

--- public/js/app.js
+++ public/js/app.js
@@ -23,31 +23,3 @@ class Model {
 }
 const app = new Model();
 export default app;
-//Object constructor to create new tasks:
-// export class Task {
-//   constructor(name, group, category, frequency, days, calander) {
-//     this.name = name;
-//     this.group = group;
-//     this.category = category;
-//     this.frequency = frequency;
-//     this.days = days;
-//     this.calander = calander;
-//     this.complete = false;
-//     app.tasks.push(this);
-//   }
-//   read() {
-//     this.complete = false;
-//     return this;
-//   }
-//   //Function to create new tasks:
-//   update(name, group, category, frequency, days, calander) {
-//     this.name = name;
-//     this.group = group;
-//     this.category = category;
-//     this.frequency = frequency;
-//     this.days = days;
-//     this.calander = calander;
-//     this.complete = false;
-//     return this;
-//   }
-// }
