49f421e66d78ca9032895f32a1c944e7fdb65beb
Mikey Nichols <mnix@journeytocode.io>
2024-02-07 21:36:37 -0500

    remove static placement of the info modal

--- public/js/views/modals/index.js
+++ public/js/views/modals/index.js
@@ -5,7 +5,6 @@ import renderAddModal, { renderModalButton } from "./add-task.js";
 const renderModals = () => {
   renderModalButton();
   renderAddModal();
-  renderTaskDetailsPopup(app.tasks[0]);
 };
 
 export default renderModals;
