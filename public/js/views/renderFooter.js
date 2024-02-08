d2c06a58cb007c813d49256720c733715d4a7255
Mikey Nichols <mnix@journeytocode.io>
2024-02-07 21:29:58 -0500

    removes extra footer from rendered footer and comments out the modal button

--- public/js/views/renderFooter.js
+++ public/js/views/renderFooter.js
@@ -1,21 +1,21 @@
 import app from "../app.js";
-import { renderModal } from "./everythingElse.temp.js";
-(() => {
-  const thisButton = app.view.createElement(
-    "div",
-    '<i class="fa-solid fa-plus add-icon"></i>',
-    document.getElementById("app"),
-    "add-icon-el",
-    "fa-solid fa-plus add-icon"
-  );
-  renderModal();
-  thisButton.onclick = () => (addButton.style.display = "block");
-})();
+
+// (() => {
+//   const thisButton = app.view.createElement(
+//     "div",
+//     '<i class="fa-solid fa-plus add-icon"></i>',
+//     document.getElementById("app"),
+//     "add-icon-el",
+//     "fa-solid fa-plus add-icon"
+//   );
+//   renderModal();
+//   thisButton.onclick = () => (addButton.style.display = "block");
+// })();
 
 const renderFooter = () => {
   app.view.createElement(
     "footer",
-    `<footer>
+    `
               <div class="footer-left">
                 <a href="https://github.com/chingu-voyages/v47-tier1-team-04">
                   <p class="copyright">© Chingu Team 04 Github</p>
@@ -27,7 +27,7 @@ const renderFooter = () => {
                   <img class="chingu-logo" src="./img/chingo-logo.png"
                     />
               </a>            
-            </footer> `,
+            `,
     document.getElementById("app"),
     "element-el",
     "footer"
