3164debcf1291662e4a372baea5d097cd7fb1794
egpennington <egpennington@hotmail.com>
2024-02-09 13:32:35 -0900

    redesign details popup, add mobile adjustments

--- public/js/views/modals/view-task.js
+++ public/js/views/modals/view-task.js
@@ -18,36 +18,41 @@ const renderTaskDetailsPopup = (task) => {
     <div class="task-details-content">
       <i class="fa-solid fa-xmark fa-2x close-details-popup"></i>
       <h2>Task Details</h2>
-      <div class="task-details">
-        <label>Task Group:</label>
-        <h3>${group}</h3>
+      <div class="task-details-group border-shadow">
+          <div class="task-details">
+            <label>Task Group:</label>
+            <h3>${group}</h3>
+          </div>
+          <div class="task-details">
+            <label>Category Name:</label
+            >
+            <h4>${category}</h4>
+          </div>
       </div>
-      <div class="task-details">
-        <label>Category Name:</label
-        >
-        <h4>${category}</h4>
-      </div>
-      <div class="task-details">
+      <div class="task-details border-shadow-sub">
         <label>Task:</label>
         <p>${name}</p>
       </div>
-      <div class="task-details">
-        <label>Frequency:</label>
-        <p>${frequency}</p>
+
+      <div class="task-details-group border-shadow-sub">
+          <div class="task-details">
+            <label>Frequency:</label>
+            <p>${frequency}</p>
+          </div>
+          <div class="task-details">
+            <label>Days:</label>
+            <p>${days}</p>
+          </div>
       </div>
-      <div class="task-details">
-        <label>Days:</label>
-        <p>${days}</p>
-      </div>
-      <div class="task-details">
+      <div class="task-details border-shadow-sub">
         <label>Calander:</label>
         <p>${calander}</p>
       </div>
-      <div class="task-details">
+      <div class="task-details border-shadow-sub">
         <label>Complete:</label>
         <p>${complete}</p>
       </div>
-      <a class="btn btn-save btn-detail" id="close-task-details"
+      <a class="btn btn-save btn-detail item-center" id="close-task-details"
         >Close</a
       >
     </div>
