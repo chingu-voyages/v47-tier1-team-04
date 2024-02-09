36acda4245963ab13960dd6ead86a61fdb81ab84
Mikey Nichols <mnix@journeytocode.io>
2024-02-09 00:41:04 -0500

    edits view-task to be more of a 'view'

--- public/js/views/modals/view-task.js
+++ public/js/views/modals/view-task.js
@@ -4,73 +4,64 @@ import { kebabCase } from "../../utilities/utilities.js";
 export const removePopup = () => {
   let popup = document.querySelector(".task-details-popup");
   if (popup) popup.remove();
-  popup = document.querySelector('.modal');
+  popup = document.querySelector(".modal");
   if (popup) popup.remove();
-  popup = document.querySelectorAll('.modal');
+  popup = document.querySelectorAll(".modal");
 };
-
 const renderTaskDetailsPopup = (task) => {
+  const { name, group, category, frequency, days, calander, complete } = task;
   removePopup();
   const detailsPopup = app.view.createElement(
     "div",
-    `<div class="task-details-popup">
-          <div class="task-details-content">
-              <i class="fa-solid fa-xmark fa-2x close-details-popup"></i>
-              <h2>Task Details</h2>
-              <div class="task-details">
-                  <label for="task_${kebabCase(task.name)}">Task Name:</label>
-                  <input type="text" value="${task.name}" id="task_${kebabCase(
-      task.name
-    )}-input" name="task_${kebabCase(task.name)}">
-              </div>
-              <div class="task-details">
-                  <label for="category_${kebabCase(
-                    task.category
-                  )}">Category Name:</label>
-                  <input type="text" value="${
-                    task.category
-                  }" id="category_${kebabCase(
-      task.category
-    )}-input" name="category_${kebabCase(task.name)}">
-              </div>
-              <div class="task-details">
-                  <label for="description-name">Description:</label>
-                  <input type="text" value="${task.name}" id="desc_${kebabCase(
-      task.name.slice(0, 20)
-    )}" name="description-name" size="50">
-              </div>
-              <div class="task-details">
-                  <label for="modal-subtask">Subtask:</label>
-                  <input type="checkbox" id="modal-subtask" name="modal-subtask" value="subtask">
-                  <input type="text" id="modal-subtask" name="modal-subtask" placeholder="Add subtask...">
-              </div>
-              <div class="task-details">
-                  <label for="modal-date">Date:</label>
-                  <input type="text" id="modal-date" name="modal-date">
-              </div>
-              <div class="task-details">
-                  <label for="modal-time">Time:</label>
-                  <input type="text" id="modal-time" name="modal-time">
-              </div>
-              <div class="task-details">
-                  <label for="priority-level">Priority:</label>
-                  <select id="priority-level" name="priority-level">
-                      <option value="low">Low</option>
-                      <option value="medium">Medium</option>
-                      <option value="high">High</option>
-                  </select>
-              </div>
-              <textarea id="notes" name="notes" class="task-details" placeholder="Notes..."></textarea>
-              <a href="#" class="btn btn-save btn-detail" id="save-task-details">Save</a>
-          </div>
-      </div>`,
+    `
+    <div class="task-details-popup">
+    <div class="task-details-content">
+      <i class="fa-solid fa-xmark fa-2x close-details-popup"></i>
+      <h2>Task Details</h2>
+      <div class="task-details">
+        <label>Task Group:</label>
+        <h3>${group}</h3>
+      </div>
+      <div class="task-details">
+        <label>Category Name:</label
+        >
+        <h4>${category}</h4>
+      </div>
+      <div class="task-details">
+        <label>Task:</label>
+        <p>${name}</p>
+      </div>
+      <div class="task-details">
+        <label>Frequency:</label>
+        <p>${frequency}</p>
+      </div>
+      <div class="task-details">
+        <label>Days:</label>
+        <p>${days}</p>
+      </div>
+      <div class="task-details">
+        <label>Calander:</label>
+        <p>${calander}</p>
+      </div>
+      <div class="task-details">
+        <label>Complete:</label>
+        <p>${complete}</p>
+      </div>
+      <a class="btn btn-save btn-detail" id="close-task-details"
+        >Close</a
+      >
+    </div>
+  </div>
+    `,
     document.getElementById("app"),
     null,
     "task-details-popup"
   ).container;
 
-  const closeDetailsButton = document.querySelector(".close-details-popup");
-  closeDetailsButton.onclick = () => detailsPopup.remove();
+  document.querySelector(".close-details-popup").onclick = () =>
+    detailsPopup.remove();
+  document.getElementById("close-task-details").onclick = () =>
+    detailsPopup.remove();
 };
 
 export default renderTaskDetailsPopup;
