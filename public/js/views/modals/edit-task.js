0ecbd784e3bdeb8aab41c4af9f65c4fd3f7d59f9
Jayanti Hari <lnu.jay@northeastern.edu>
2024-02-12 00:59:36 -0500

    updated checkbox values

--- public/js/views/modals/edit-task.js
+++ public/js/views/modals/edit-task.js
@@ -61,27 +61,27 @@ const renderEditTaskDetailsPopup = (task) => {
                       <label for="Monday">Mon</label>
                   </div>
                   <div class="checkbox-container">
-                      <input type="checkbox" id="Tuesday" name="Tuesday" value="Tuesday" class="days" checked = ${task.days.includes("Tuesday")}>
+                      <input type="checkbox" id="Tuesday" name="Tuesday" value="Tuesday" class="days" ${task.days.includes("Tuesday") ? checked : ''}>
                       <label for="Tuesday">Tue</label>
                   </div>
                   <div class="checkbox-container">
-                      <input type="checkbox" id="Wednesday" name="Wednesday" value="Wednesday" class="days" checked = ${task.days.includes("Wednesday")}>
+                      <input type="checkbox" id="Wednesday" name="Wednesday" value="Wednesday" class="days" ${task.days.includes("Wednesday") ? checked : ''}>
                       <label for="Wednesday">Wed</label>
                   </div>
                   <div class="checkbox-container">
-                      <input type="checkbox" id="Thursday" name="Thursday" value="Thursday" class="days" checked = ${task.days.includes("Thursday")}>
+                      <input type="checkbox" id="Thursday" name="Thursday" value="Thursday" class="days" ${task.days.includes("Thursday") ? checked : ''}>
                       <label for="Thursday">Thur</label>
                   </div>
                   <div class="checkbox-container">
-                      <input type="checkbox" id="Friday" name="Friday" value="Friday" class="days" checked = ${task.days.includes("Friday")}>
+                      <input type="checkbox" id="Friday" name="Friday" value="Friday" class="days" ${task.days.includes("Friday") ? checked : ''}>
                       <label for="Friday">Fri</label>
                   </div>
                   <div class="checkbox-container">
-                      <input type="checkbox" id="Saturday" name="Saturday" value="Saturday" class="days" checked = ${task.days.includes("Saturday")}>
+                      <input type="checkbox" id="Saturday" name="Saturday" value="Saturday" class="days" ${task.days.includes("Saturday") ? checked : ''}>
                       <label for="Saturday">Sat</label>
                   </div>
                   <div class="checkbox-container">
-                      <input type="checkbox" id="Sunday" name="Sunday" value="Sunday" class="days" checked = ${task.days.includes("Sunday")}>
+                      <input type="checkbox" id="Sunday" name="Sunday" value="Sunday" class="days" ${task.days.includes("Sunday") ? checked : ''}>
                       <label for="Sunday">Sun</label>
                   </div>
           </div>
