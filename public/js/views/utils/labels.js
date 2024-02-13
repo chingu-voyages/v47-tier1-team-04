2473d6d17af16370544335a7430bf1cbd5048afc
Mikey Nichols <mnix@journeytocode.io>
2024-02-12 22:24:42 -0500

    adds strings to numbers in labels

--- public/js/views/utils/labels.js
+++ public/js/views/utils/labels.js
@@ -1,14 +1,12 @@
 const label = (priority) => {
-  console.log(priority)
   switch (priority) {
-    case 3:
-      console.log(priority)
+    case '3':
       return `<a href="#" class="btn btn-lite btn-blue">Low</a>`;
 
-    case 2:
+    case '2':
       return `<a href="#" class="btn btn-lite btn-orange">Med</a>`;
 
-    case 1:
+    case '1':
       return `<a href="#" class="btn btn-lite btn-red">High</a>`;
 
     default:
