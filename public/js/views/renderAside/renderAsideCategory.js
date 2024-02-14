991a0b2940087e7ccec594ca9d89d9824937514e
stacy_r <stacyrileyca@gmail.com>
2024-02-14 15:34:13 -0500

    Imported kebabCase and explained functionality

--- public/js/views/renderAside/renderAsideCategory.js
+++ public/js/views/renderAside/renderAsideCategory.js
@@ -1,9 +1,10 @@
 import app from "../../app.js";
+import { kebabCase } from "../../utilities/utilities.js";
 const renderAsideCategory = (category, group) => {
   app.view.createElement(
     "li",
     category,
-    document.getElementById(`sidebar_${group}`)
+    document.getElementById(`sidebar_${kebabCase(group)}`)
   );
 };
 export default renderAsideCategory;
