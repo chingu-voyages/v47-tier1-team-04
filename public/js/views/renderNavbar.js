a8bc5362d83a3c611abd3f6565981ce33a5e0d97
Mikey Nichols <mnix@journeytocode.io>
2024-02-13 20:54:08 -0500

    adds logic to display which sort feature is being utilized

--- public/js/views/renderNavbar.js
+++ public/js/views/renderNavbar.js
@@ -11,31 +11,34 @@ const renderNavBar = () => {
                 </div>
             </div>            
             <div class="navbtn">
-                <a id="btn-day" class="btn-day">Today</a>
-                <a id="btn-month" class="btn-month">Month</a>
-                <a id="btn-all" class="btn-year">All</a>  
+                <a id="btn-day" class="btn btn-day">Today</a>
+                <a id="btn-month" class="btn btn-month">Month</a>
+                <a id="btn-all" class="btn btn-year active">All</a>  
             </div>
             <div class="navday">
-                <a id="btn-mon" class="btn-week">Monday</a>
-                <a id="btn-tue" class="btn-week">Tuesday</a>
-                <a id="btn-wed" class="btn-week">Wednesday</a> 
-                <a id="btn-thur" class="btn-week">Thursday</a>      
-                <a id="btn-fri" class="btn-week">Friday</a>  
-                <a id="btn-sat" class="btn-week">Saturday</a>  
-                <a id="btn-sun" class="btn-week">Sunday</a>  
+                <a id="btn-mon" class="btn btn-week">Monday</a>
+                <a id="btn-tue" class="btn btn-week">Tuesday</a>
+                <a id="btn-wed" class="btn btn-week">Wednesday</a> 
+                <a id="btn-thur" class="btn btn-week">Thursday</a>      
+                <a id="btn-fri" class="btn btn-week">Friday</a>  
+                <a id="btn-sat" class="btn btn-week">Saturday</a>  
+                <a id="btn-sun" class="btn btn-week">Sunday</a>  
             </div>
             `,
     document.getElementById("app"),
     "element-el",
     "navbar"
   );
+  app.filteredTasks = app.tasks;
 
-  document.querySelectorAll('.btn-week').forEach(ele => {
+  document.querySelectorAll(".btn.btn-week").forEach((ele) => {
     ele.onclick = (e) => {
-      renderFilteredTasks(app.tasks.filter((task) => task.days.includes(e.target.innerHTML)));
-    }
-  
-  })
+      resetButtons(e.target);
+      renderFilteredTasks(
+        app.tasks.filter((task) => task.days.includes(e.target.innerHTML))
+      );
+    };
+  });
   function formatDate(date) {
     let d = new Date(date),
       month = "" + (d.getMonth() + 1),
@@ -47,17 +50,24 @@ const renderNavBar = () => {
 
     return [year, month, day].join("-");
   }
-  document.getElementById("btn-all").onclick = () => updateContent();
-  document.getElementById("btn-month").onclick = () => {
+  document.getElementById("btn-all").onclick = (e) => {
+    resetButtons(e.target);
+    updateContent(app.tasks);
+  };
+  document.getElementById("btn-month").onclick = (e) => {
+    resetButtons(e.target);
     renderFilteredTasks(
       app.tasks.filter((task) => {
         if (typeof task.date === "undefined") return;
-        return task.date.slice(0, 7) ===
-          formatDate(new Date().toString().slice(0, 15)).slice(0, 7);
+        return (
+          task.date.slice(0, 7) ===
+          formatDate(new Date().toString().slice(0, 15)).slice(0, 7)
+        );
       })
     );
   };
-  document.getElementById("btn-day").onclick = () => {
+  document.getElementById("btn-day").onclick = (e) => {
+    resetButtons(e.target);
     renderFilteredTasks(
       app.tasks.filter(
         (task) => task.date === formatDate(new Date().toString().slice(0, 15))
@@ -70,10 +80,10 @@ const renderNavBar = () => {
     <div class="priority">
         <a id="priority_low" class="btn btn-lite btn-blue">Low</a>
         <a id="priority_med" class="btn btn-lite btn-orange">Med</a>
-        <a id="high" class="btn btn-lite btn-red">High</a>
+        <a id="priority_high" class="btn btn-lite btn-red">High</a>
 
         <div class="search">
-            <input type="text" placeholder="Search by task name, category, group, etc..." id="search">
+            <input type="text" placeholder="Search by task name, category, group, etc..." id="search" class="btn">
             <i class="fa-solid fa-magnifying-glass fa-lg search-icon"></i>
         </div>
     </div>               
@@ -83,9 +93,21 @@ const renderNavBar = () => {
     "contentAnchor",
     "content"
   );
-  document.getElementById("search").addEventListener("input", (e) => {
+  const searchBar = document.getElementById("search");
+  const buttons = document.querySelectorAll(".btn");
+  const resetButtons = (target) => {
+    if (target !== searchBar) {
+      searchBar.value = "";
+      searchBar.classList.remove("active");
+    }
+    buttons.forEach((ele) => {
+      ele.classList.remove("active");
+    });
+    target.classList.toggle("active");
+  };
+  searchBar.addEventListener("input", (e) => {
     let searchKey = e.target.value.toLowerCase();
-
+    resetButtons(searchBar);
     let tasks = app.tasks.filter((task) => {
       const description = (search) => {
         if (task.description) {
@@ -108,11 +130,17 @@ const renderNavBar = () => {
     .getElementById("save-all")
     .addEventListener("click", app.controller.saveData);
 
-  document.getElementById("priority_low").onclick = () =>
+  document.getElementById("priority_low").onclick = (e) => {
+    resetButtons(e.target);
     renderFilteredTasks(app.tasks.filter((task) => task.priority == 3));
-  document.getElementById("priority_med").onclick = () =>
+  };
+  document.getElementById("priority_med").onclick = (e) => {
+    resetButtons(e.target);
     renderFilteredTasks(app.tasks.filter((task) => task.priority == 2));
-  document.getElementById("high").onclick = () =>
+  };
+  document.getElementById("priority_high").onclick = (e) => {
+    resetButtons(e.target);
     renderFilteredTasks(app.tasks.filter((task) => task.priority == 1));
+  };
 };
 export default renderNavBar;
