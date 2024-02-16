e34ba19c4ab7e39b5212b1bffe885e554f9a595e
Mikey Nichols <mnix@journeytocode.io>
2024-02-16 01:30:51 -0500

    creates a toast element and renders it on successful save

--- public/js/views/utils/save.js
+++ public/js/views/utils/save.js
@@ -1,6 +1,13 @@
-  //Popup message to user once they press the "save" button:
-  const renderSuccessfulSave = () => {
-    alert("Your data has been saved!");
-  }
+// Create a toast element
+const toast = document.createElement("div");
+toast.className = "toast";
+document.body.appendChild(toast);
 
-  export default renderSuccessfulSave
+// Function to show a toast message
+const renderSuccessfulSave = () => {
+  toast.textContent = "Your data has been saved!";
+  toast.classList.add("show");
+  setTimeout(() => toast.classList.remove("show"), 4000);
+};
+
+export default renderSuccessfulSave;
