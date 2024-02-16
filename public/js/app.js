c72abcce7ecdbeb670b522d35325ceb2195e3331
Mikey Nichols <mnix@journeytocode.io>
2024-02-16 02:13:07 -0500

    renames controller to Controller

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,10 @@
-import Controller from "./controller.js";
-import View from './views/view.index.js';
+import Controller from "./Controller.js";
+import View from "./views/View.index.js";
 
 class Model {
   constructor() {
     this.tasks = []; //Creates an empty tasks array
-    this.archive = [];//Creates an empty archive
+    this.archive = []; //Creates an empty archive
     this.view = new View(
       "div",
       "<!--Code injected by Amanda Team 4-->",
@@ -12,15 +12,14 @@ class Model {
       "app",
       "container"
     ); //Allows us to put the app onto the screen by using above parameters and gives us access to app.view.method()
-    this.controller = new Controller();//Allows us to access the Controller by adding it here, don't have to import on each page
+    this.controller = new Controller(); //Allows us to access the Controller by adding it here, don't have to import on each page
   }
   //Function to initialize app:
   async init(title) {
-    await this.controller.loadData();//Line runs process to check for localStorage and then makes tasks array from that or seed data
-    this.controller.init(title);//
+    await this.controller.loadData(); //Line runs process to check for localStorage and then makes tasks array from that or seed data
+    this.controller.init(title); //
     return this;
   }
 }
 const app = new Model(); //Creating a new data model and naming it 'app'
 export default app;
-
