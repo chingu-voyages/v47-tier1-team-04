9c77e51fb5dcb68322ba3512b1300b5645586a0c
Mikey Nichols <mnix@journeytocode.io>
2024-02-15 19:57:17 -0500

    adds additional check of quotation to the kebabCase function

--- public/js/utilities/utilities.js
+++ public/js/utilities/utilities.js
@@ -3,6 +3,8 @@ export const kebabCase = (str) =>
     .replace(/([a-z])([A-Z])/g, "$1-$2")
     .replace(/[\s_]+/g, "-")
     .toLowerCase()
+    .replaceAll('\'','')
+    .replaceAll('+','_')
     .replaceAll('/','-')
     .replaceAll(':','-'); // function for applying kebab-case to a string
 export const resetView = (i = 0) => (i = 1); // function for reseting the view index on demand
