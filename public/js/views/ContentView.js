1cc9cdf6661b9db7a808a53000ea8e2c646de14e
Mikey Nichols <mnix@journeytocode.io>
2024-02-21 20:53:45 -0500

    Refactor content rendering and add group event listeners

--- public/js/views/ContentView.js
+++ public/js/views/ContentView.js
@@ -31,7 +31,9 @@ export default class ContentViewController {
 
   // Clears the content of a specific group
   clearContentGroup(group) {
-    const groupElement = document.getElementById(`content_${this.formatString(group)}`);
+    const groupElement = document.getElementById(
+      `content_${this.formatString(group)}`
+    );
     if (groupElement) groupElement.innerHTML = "";
     else this.clearContent();
   }
@@ -45,7 +47,13 @@ export default class ContentViewController {
 
   // Renders a specific content group
   renderContentGroup(group) {
-    this.createElement("div", `<h2 class="category-name">${group}</h2>`, "content", `content_${this.formatString(group)}`, "content-activity");
+    this.createElement(
+      "div",
+      `<h2 class="group-name">${group}</h2>`,
+      "content",
+      `content_${this.formatString(group)}`,
+      "content-activity"
+    );
   }
 
   // Renders all content groups
@@ -58,7 +66,9 @@ export default class ContentViewController {
 
   // Clears the content of a specific category
   clearContentCategory(category) {
-    document.getElementById(`category_${this.formatString(category)} .content-inner`).innerHTML = "";
+    document.getElementById(
+      `category_${this.formatString(category)} .content-inner`
+    ).innerHTML = "";
   }
 
   // Updates the content of a specific category
@@ -70,14 +80,27 @@ export default class ContentViewController {
   // Renders a specific content category
   renderContentCategory(group, category) {
     const catTasks = app.tasks.filter((task) => task.category === category);
-    const completeCategory = catTasks.filter((task) => task.complete).length === catTasks.length;
-    this.createElement("div", this.createContentMainHTML(group, category, completeCategory), `content_${this.formatString(group)}`, `category_${this.formatString(category)}`);
+    const completeCategory =
+      catTasks.filter((task) => task.complete).length === catTasks.length;
+    this.createElement(
+      "div",
+      this.createContentMainHTML(group, category, completeCategory),
+      `content_${this.formatString(group)}`,
+      `category_${this.formatString(category)}`
+    );
     this.addTaskEventListeners();
+    this.addGroupEventListeners();
   }
 
   // Creates an HTML element with the specified parameters
   createElement(tag, innerHTML, parentId, id, className) {
-    app.view.createElement(tag, innerHTML, document.getElementById(parentId), id, className);
+    app.view.createElement(
+      tag,
+      innerHTML,
+      document.getElementById(parentId),
+      id,
+      className
+    );
   }
 
   // Formats a string by calling the formatString method of the controller
@@ -87,14 +110,20 @@ export default class ContentViewController {
 
   // Renders all categories of a specific group
   renderCategories(group) {
-    app.controller.returnUniqueCategoriesByGroup(group).map((category) => this.renderContentCategory(group, category));
+    app.controller
+      .returnUniqueCategoriesByGroup(group)
+      .map((category) => this.renderContentCategory(group, category));
   }
 
   // Creates the main HTML content for a specific group and category
   createContentMainHTML(group, category, completeCategory) {
     return `
       <div class="content-main">
-          <img src="${completeCategory ? `./img/favicon.png` : `./img/Ellipse8.svg`}" alt="ellipse checkbox" class="ellipse" id="ellipse_${this.formatString(group)}_${this.formatString(category)}">
+          <img src="${
+            completeCategory ? `./img/favicon.png` : `./img/Ellipse8.svg`
+          }" alt="ellipse checkbox" class="ellipse" id="ellipse_${this.formatString(
+      group
+    )}_${this.formatString(category)}">
           <div class="content-inner ${completeCategory ? `darken` : ""}" id="">
               <div class="content-task">
                   <h3 class="activity">${category}</h3> 
@@ -105,14 +134,29 @@ export default class ContentViewController {
       </div>               
     `;
   }
-
+  addGroupEventListeners() {
+    document.querySelectorAll(".group-name").forEach((ele) => {
+      ele.onclick = (e) => {
+        for (let contentInner of e.target.parentElement.children) {
+          console.log(contentInner.classList)
+          if (contentInner.classList.contains("group-name")) {
+            contentInner.classList.toggle("group-name-active")
+          } else {
+            contentInner.classList.toggle("hide");
+          }
+        }
+      };
+    });
+  }
   // Adds event listeners to all elements with the class 'add-task'
   addTaskEventListeners() {
     document.querySelectorAll(".add-task").forEach((ele) => {
       ele.onclick = (e) => {
         ele.style.display = "none";
         const category = e.target.previousElementSibling.innerHTML;
-        const group = e.target.parentElement.parentElement.parentElement.parentElement.parentElement.firstChild.innerHTML;
+        const group =
+          e.target.parentElement.parentElement.parentElement.parentElement
+            .parentElement.firstChild.innerHTML;
         const inputElement = document.createElement("input");
         inputElement.placeholder = "enter quick task here";
         inputElement.setAttribute("type", "text");
