d7322a5bab4fec6061c21162f563278066e5d5c6
Mikey Nichols <mnix.dev@gmail.com>
2024-02-26 01:34:46 -0500

    Add Chingu logo and test message

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -2,15 +2,17 @@
 // import Aside from './components/aside';
  // // // //import NavBar from './components/NavBar';
 // // // import Content from './components/content';
-import Footer from './components/Footer';
-
+// import Footer from './components/Footer';
+import chinguLogo from './images/chingo-logo.png';
 function App() {
   return (
     <div className="App">
       {/* <Aside /> */}
       {/* <NavBar /> */}
+      <h1>Testing 1234</h1>
+      <img src={chinguLogo} alt="Chingu Logo" />
        {/* <Content /> */}
-     <Footer />
+     {/* <Footer /> */}
     </div>
   );
 }
