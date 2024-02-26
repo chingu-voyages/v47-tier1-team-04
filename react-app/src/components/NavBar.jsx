ee0d9100169b81389aa94733eebf0844dfc24133
Mikey Nichols <mnix.dev@gmail.com>
2024-02-25 22:12:28 -0500

    Add NavBar component

---
+++ react-app/src/components/NavBar.jsx
@@ -0,0 +1,59 @@
+function NavBar() {
+    return (
+        <nav id="element-el" className="navbar">
+            <div className="navbar">
+                <div className="navbar-top">
+                    <i className="fa-solid fa-bars menu-btn fa-2x" id="menu-btn"></i>
+                    <div id="date" className="date">
+                        Today: 2/25/2024
+                    </div>
+                    <div className="btn-undo">
+                        <a id="mode-switch" href="#" className="btn btn-mode-switch">
+                            <i className="fa-solid fa-circle-half-stroke"></i>
+                        </a>
+                        <a id="save-all" href="#" className="btn btn-save">
+                            Save
+                        </a>
+                    </div>
+                </div>
+
+                <div className="navbtn">
+                    <a id="btn-day" className="btn btn-day">
+                        Today
+                    </a>
+                    <a id="btn-month" className="btn btn-month">
+                        Month
+                    </a>
+                    <a id="btn-all" className="btn btn-year active">
+                        All
+                    </a>
+                </div>
+                <div className="navday">
+                    <a id="btn-mon" className="btn btn-week">
+                        Monday
+                    </a>
+                    <a id="btn-tue" className="btn btn-week">
+                        Tuesday
+                    </a>
+                    <a id="btn-wed" className="btn btn-week">
+                        Wednesday
+                    </a>
+                    <a id="btn-thu" className="btn btn-week">
+                        Thursday
+                    </a>
+                    <a id="btn-fri" className="btn btn-week">
+                        Friday
+                    </a>
+                    <a id="btn-sat" className="btn btn-week">
+                        Saturday
+                    </a>
+                    <a id="btn-sun" className="btn btn-week">
+                        Sunday
+                    </a>
+                </div>
+            </div>
+        </nav>
+    );
+}
+
+export default NavBar;
