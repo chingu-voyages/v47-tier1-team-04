406f3aecf7b535d5eb12a2bcb63a347ac84c8733
Mikey Nichols <mnix.dev@gmail.com>
2024-02-26 01:48:18 -0500

    Commented out Aside component in App.js

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,5 +1,5 @@
 //import './styles/css/main.css';
-import Aside from './components/aside';
+// import Aside from './components/aside';
 // import NavBar from './components/NavBar';
 // // // import Content from './components/content';
 // import Footer from './components/Footer';
@@ -7,7 +7,7 @@ import chinguLogo from './images/chingo-logo.png';
 function App() {
   return (
     <div className="App">
-      <Aside />
+      {/* <Aside /> */}
       {/* <NavBar /> */}
       <h1>Testing 1234</h1>
       <img src={chinguLogo} alt="Chingu Logo" />
