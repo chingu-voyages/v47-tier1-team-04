f712938b8e2f204145b0724259635fa0fc018742
Jayanti Hari <lnu.jay@northeastern.edu>
2024-02-25 23:04:14 -0500

    adds initial aside

---
+++ react-app/src/components/aside.jsx
@@ -0,0 +1,23 @@
+import avatar from './images/img/Friendly Ones Avatar and Backdrop.png';
+import settingsicon from './images/img/solar_settings.svg';
+
+function Aside () {
+    return (
+        <aside id="aside-el" class="aside">
+            <div class="avatar-area">
+                <div class="avatar">
+                <img src={avatar} alt="avatar pict" className="avatar-pict" />
+                </div>
+                <div class="gear-icon">
+                <img src={settingsicon} alt="gear icon" id="settings-icon" />
+                </div>
+            </div>            
+            <h2>Daily Tasks</h2>
+            <div id="daily-checklist"><div id="view_15">
+            <h3 id="sidebar_group_grop"><a href="#content_grop"> grop </a><i class="fa-solid fa-circle-chevron-down"></i></h3>
+            <ul id="sidebar_grop"><li id="view_16"><a href="#category_cat">cat</a></li></ul>
+        </div>
+        </div>
+    </aside>
+    )
+}
