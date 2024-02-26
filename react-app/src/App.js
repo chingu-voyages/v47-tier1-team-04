d555821c7adac4a307c28909e8695782920cdb62
Mikey Nichols <mnix.dev@gmail.com>
2024-02-26 01:23:48 -0500

    tries rendering content

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,17 +1,16 @@
 import './styles/css/main.css';
 // import Aside from './components/aside';
-// // // // //import NavBar from './components/NavBar';
-// import Content from './components/content';
+ // // // //import NavBar from './components/NavBar';
+import Content from './components/content';
 // import Footer from './components/Footer';
 
 function App() {
-  console.log('testing')
   return (
     <div className="App">
       {/* <Aside /> */}
-      <NavBar />
-      {/* <Content />
-      <Footer /> */}
+      {/* <NavBar /> */}
+       <Content />
+     {/* <Footer /> */}
     </div>
   );
 }
