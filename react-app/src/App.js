5ff551be5121aed3465819c811d2ee83248a5d87
Mikey Nichols <mikenichols410@gmail.com>
2024-02-26 22:20:52 -0500

    Refactor App component to use class syntax and add dark mode toggle

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,3 +1,4 @@
+import { Component } from "react";
 import "./styles/css/main.css";
 import Aside from "./components/Aside";
 import NavBar from "./components/NavBar";
@@ -5,16 +6,32 @@ import Content from "./components/Content";
 import Footer from "./components/Footer";
 import AddTaskButton from "./components/AddTaskButton";
 
-function App() {
-  return (
-    <div className="App">
-      <Aside />
-      <NavBar />
-      <Content />
-      <Footer />
-      <AddTaskButton />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      darkMode: false,
+    };
+  }
+  toggleDarkMode = () => {
+    this.setState({ darkMode: !this.state.darkMode });
+    if (this.state.darkMode) {
+      document.body.classList.add("dark-mode");
+    } else {
+      document.body.classList.remove("dark-mode");
+    }
+  };
+  render() {
+    return (
+      <div className="container">
+        <Aside />
+        <NavBar toggleDarkMode={this.toggleDarkMode} />
+        <Content />
+        <Footer />
+        <AddTaskButton />
+      </div>
+    );
+  }
 }
 
 export default App;
