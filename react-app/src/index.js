e69bbed15e04382a78bd855b4401f09ef4e32fe4
Mikey Nichols <mikenichols410@gmail.com>
2024-02-28 09:33:34 -0500

    Update CSS imports in index.js

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import "./styles/css/main.css";
+import '@fortawesome/fontawesome-free/css/all.min.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
