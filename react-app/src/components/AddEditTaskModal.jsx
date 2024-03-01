7a8d80f39db25560415b030b4c5e1b17e8ae6975
Mikey Nichols <mikenichols410@gmail.com>
2024-02-29 23:46:49 -0500

    reformats file

--- react-app/src/components/AddEditTaskModal.jsx
+++ react-app/src/components/AddEditTaskModal.jsx
@@ -1,9 +1,13 @@
-function AddEditModal({ closeModal}) {
+function AddEditModal({ closeModal }) {
   return (
     <div id="edit-task-details-popup" className="task-details-popup">
       <form className="task-details-popup" id="edit_details">
         <div className="task-details-content">
-        <i onClick={() => closeModal()} className="fa-solid fa-xmark fa-2x close-details-popup" id="close-details-popup"></i>
+          <i
+            onClick={() => closeModal()}
+            className="fa-solid fa-xmark fa-2x close-details-popup"
+            id="close-details-popup"
+          ></i>
           <h2>Add Task</h2>
           <div className="task-details-group">
             <div className="task-details">
@@ -53,83 +57,83 @@ function AddEditModal({ closeModal}) {
               />
             </div>
           </div>
-        <div className="block">
+          <div className="block">
             <label className="block" htmlFor="Monday">
-                Days:
+              Days:
             </label>
             <div className="day-checkboxes" id="checkboxes">
-                <div className="checkbox-container">
-                    <input
-                        type="checkbox"
-                        id="Monday"
-                        name="Monday"
-                        value="Monday"
-                        className="days"
-                    />
-                    <label htmlFor="Monday">Mon</label>
-                </div>
-                <div className="checkbox-container">
-                    <input
-                        type="checkbox"
-                        id="Tuesday"
-                        name="Tuesday"
-                        value="Tuesday"
-                        className="days"
-                    />
-                    <label htmlFor="Tuesday">Tue</label>
-                </div>
-                <div className="checkbox-container">
-                    <input
-                        type="checkbox"
-                        id="Wednesday"
-                        name="Wednesday"
-                        value="Wednesday"
-                        className="days"
-                    />
-                    <label htmlFor="Wednesday">Wed</label>
-                </div>
-                <div className="checkbox-container">
-                    <input
-                        type="checkbox"
-                        id="Thursday"
-                        name="Thursday"
-                        value="Thursday"
-                        className="days"
-                    />
-                    <label htmlFor="Thursday">Thur</label>
-                </div>
-                <div className="checkbox-container">
-                    <input
-                        type="checkbox"
-                        id="Friday"
-                        name="Friday"
-                        value="Friday"
-                        className="days"
-                    />
-                    <label htmlFor="Friday">Fri</label>
-                </div>
-                <div className="checkbox-container">
-                    <input
-                        type="checkbox"
-                        id="Saturday"
-                        name="Saturday"
-                        value="Saturday"
-                        className="days"
-                    />
-                    <label htmlFor="Saturday">Sat</label>
-                </div>
-                <div className="checkbox-container">
-                    <input
-                        type="checkbox"
-                        id="Sunday"
-                        name="Sunday"
-                        value="Sunday"
-                        className="days"
-                    />
-                    <label htmlFor="Sunday">Sun</label>
-                </div>
+              <div className="checkbox-container">
+                <input
+                  type="checkbox"
+                  id="Monday"
+                  name="Monday"
+                  value="Monday"
+                  className="days"
+                />
+                <label htmlFor="Monday">Mon</label>
+              </div>
+              <div className="checkbox-container">
+                <input
+                  type="checkbox"
+                  id="Tuesday"
+                  name="Tuesday"
+                  value="Tuesday"
+                  className="days"
+                />
+                <label htmlFor="Tuesday">Tue</label>
+              </div>
+              <div className="checkbox-container">
+                <input
+                  type="checkbox"
+                  id="Wednesday"
+                  name="Wednesday"
+                  value="Wednesday"
+                  className="days"
+                />
+                <label htmlFor="Wednesday">Wed</label>
+              </div>
+              <div className="checkbox-container">
+                <input
+                  type="checkbox"
+                  id="Thursday"
+                  name="Thursday"
+                  value="Thursday"
+                  className="days"
+                />
+                <label htmlFor="Thursday">Thur</label>
+              </div>
+              <div className="checkbox-container">
+                <input
+                  type="checkbox"
+                  id="Friday"
+                  name="Friday"
+                  value="Friday"
+                  className="days"
+                />
+                <label htmlFor="Friday">Fri</label>
+              </div>
+              <div className="checkbox-container">
+                <input
+                  type="checkbox"
+                  id="Saturday"
+                  name="Saturday"
+                  value="Saturday"
+                  className="days"
+                />
+                <label htmlFor="Saturday">Sat</label>
+              </div>
+              <div className="checkbox-container">
+                <input
+                  type="checkbox"
+                  id="Sunday"
+                  name="Sunday"
+                  value="Sunday"
+                  className="days"
+                />
+                <label htmlFor="Sunday">Sun</label>
+              </div>
             </div>
-        </div>
+          </div>
           <div className="task-details-group task-details-due">
             <div className="task-details">
               <label htmlFor="frequency">Frequency:</label>
