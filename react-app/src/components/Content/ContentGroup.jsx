ac6da71e87006d43810d599c794a118478f142ba
Mikey Nichols <mikenichols410@gmail.com>
2024-03-03 16:55:31 -0500

    adds h2 back to groups and also sets their id so we can click and navigate to them

--- react-app/src/components/Content/ContentGroup.jsx
+++ react-app/src/components/Content/ContentGroup.jsx
@@ -1,8 +1,11 @@
+import formatString from "../../utils/formatString";
 import ContentCategory from "./ContentCategory";
 function ContentGroup({ groupTasks }) {
   const categories = [...new Set(groupTasks.map((task) => task.category))];
+  const groupTask = groupTasks[0].group;
   return (
-    <div className="content-activity">
+    <div className="content-activity" id={`group_${formatString(groupTask)}`}>
+      <h2 class="group-name">{groupTask} <i className="fa-solid"></i> <i className="fa fa-solid fa-edit"></i></h2>
       {categories.map((category, index) => {
         const categoryTasks = groupTasks.filter(
           (task) => task.category === category
