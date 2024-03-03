eb591dd2ae57669891b74b436f92c4715b5d5897
Mikey Nichols <mikenichols410@gmail.com>
2024-03-03 16:18:17 -0500

    Remove unused font-awesome and apple-touch-icon references

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -86,18 +86,6 @@ const App = () => {
           href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
           rel="stylesheet"
         />
-        <link
-          rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
-          integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
-          crossorigin="anonymous"
-          referrerpolicy="no-referrer"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="./apple-touch-icon.png"
-        />
         <title>{title}</title>
       </Helmet>
       <Aside
