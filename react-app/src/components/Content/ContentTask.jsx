6a039f7f2d54bbeb6cf9c03c3bfac9ce9ea15ab8
Mikey Nichols <mikenichols410@gmail.com>
2024-03-03 20:13:47 -0500

    adds on click function to use when user clicks trash can

--- react-app/src/components/Content/ContentTask.jsx
+++ react-app/src/components/Content/ContentTask.jsx
@@ -1,7 +1,7 @@
 import pencil from "../../images/mynaui_pencil.svg";
 import trash from "../../images/ph_trash.svg";
 
-function ContentTask({ task }) {
+function ContentTask({ task, archiveTask }) {
   return (
     <div className="content-description">
       <p className={`task-name ${task.complete ? "complete" : ""}`}>
@@ -19,7 +19,7 @@ function ContentTask({ task }) {
         />
         <i className="fa-solid fa-circle-info fa-2x detail" />
         <img src={pencil} alt="edit pencil" className="icon-update" />
-        <img src={trash} alt="delete trash can" className="icon-edit" />
+        <img src={trash} alt="delete trash can" className="icon-edit" onClick={() => archiveTask(task)}/>
       </div>
     </div>
   );
