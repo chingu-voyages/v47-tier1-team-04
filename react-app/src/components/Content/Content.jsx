2a2ad2c24767b3f3ea8b34c14517a1e02a120868
Mikey Nichols <mikenichols410@gmail.com>
2024-03-05 16:53:16 -0500

    refactors component to use state and effect hooks

--- react-app/src/components/Content/Content.jsx
+++ react-app/src/components/Content/Content.jsx
@@ -1,11 +1,29 @@
+import { useState, useEffect } from "react";
 import ContentGroup from "./ContentGroup";
-function Content({ tasks, archiveTask, updateTask}) {
-  const groups = [...new Set(tasks.map((task) => task.group))]
+function Content({ saveData, tasks, archiveTask, updateTask, updateGroups }) {
+  const [groups, setGroups] = useState([...new Set(tasks.map((task) => task.group))]);
+  useEffect(() => {
+    setGroups([...new Set(tasks.map((task) => task.group))]);
+  }, [tasks]);
   return (
     <div id="contentAnchor" className="content">
       {groups.map((group, index) => {
         const groupTasks = tasks.filter((task) => task.group === group);
-        return <ContentGroup key={index} groupTasks={groupTasks} archiveTask={archiveTask} tasks={tasks} updateTask={updateTask} />;
+        
+        return (
+          <ContentGroup
+            key={index}
+            groups={groups}
+            updateGroups={updateGroups}
+            group={group}
+            groupTasks={groupTasks}
+            archiveTask={archiveTask}
+            tasks={tasks}
+            updateTask={updateTask}
+            setGroups={setGroups}
+            saveData={saveData}
+          />
+        );
       })}
     </div>
   );
