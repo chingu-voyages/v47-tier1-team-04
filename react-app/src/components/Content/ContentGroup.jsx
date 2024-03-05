68fec45f186e185042d7302275443f62f1705195
Mikey Nichols <mikenichols410@gmail.com>
2024-03-05 16:54:37 -0500

    Refactor ContentGroup component to use useState and useEffect hooks

--- react-app/src/components/Content/ContentGroup.jsx
+++ react-app/src/components/Content/ContentGroup.jsx
@@ -1,16 +1,48 @@
+import { useState, useEffect } from "react";
 import formatString from "../../utils/formatString";
 import ContentCategory from "./ContentCategory";
-function ContentGroup({ groupTasks, archiveTask, tasks, updateTask }) {
-  const categories = [...new Set(groupTasks.map((task) => task.category))];
-  const groupTask = groupTasks[0].group;
+function ContentGroup({
+  saveData,
+  setGroups,
+  group,
+  archiveTask,
+  tasks,
+  updateTask,
+  updateGroups
+}) {
+  const [groupTasks, setGroupTasks] = useState(
+    tasks.filter((task) => task.group === group)
+  );
+  const [categories, setCategories] = useState([...new Set(groupTasks.map((task) => task.category))])
+   const groupStr = groupTasks[0].group;
+  useEffect(() => {
+    updateGroups();
+    setGroups([...new Set(tasks.map((task) => task.group))])
+  }, [groupTasks]);
   return (
-    <div className="content-activity" id={`group_${formatString(groupTask)}`}>
-      <h2 class="group-name">{groupTask} <i className="fa-solid"></i> <i className="fa fa-solid fa-edit"></i></h2>
+    <div className="content-activity" id={`group_${formatString(groupStr)}`}>
+      <h2 className="group-name">
+        {groupStr} <i className="fa-solid"></i>{" "}
+        <i className="fa fa-solid fa-edit"></i>
+      </h2>
       {categories.map((category, index) => {
         const categoryTasks = groupTasks.filter(
           (task) => task.category === category
         );
-        return <ContentCategory key={index} categoryTasks={categoryTasks} archiveTask={archiveTask} updateTask={updateTask} tasks={tasks} />;
+        return (
+          <ContentCategory
+          updateGroups={updateGroups}
+            saveData={saveData}
+            key={index}
+            groupTasks={groupTasks}
+            setGroupTasks={setGroupTasks}
+            setGroups={setGroups}
+            categoryTasks={categoryTasks}
+            archiveTask={archiveTask}
+            updateTask={updateTask}
+            tasks={tasks}
+          />
+        );
       })}
     </div>
   );
