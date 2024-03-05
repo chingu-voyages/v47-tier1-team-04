f7a13b643bfc966da395cf6a3249b956a7a9b98c
Mikey Nichols <mikenichols410@gmail.com>
2024-03-05 16:54:11 -0500

    refactors to use state

--- react-app/src/components/Content/ContentCategory.jsx
+++ react-app/src/components/Content/ContentCategory.jsx
@@ -1,25 +1,42 @@
+import { useState } from "react";
 import ContentTask from "./ContentTask";
 import ellipse from "../../images/Ellipse8.svg";
 import formatString from "../../utils/formatString";
 
-function ContentCategory({ categoryTasks, archiveTask, updateTask, tasks }) {
-  const categoryTask = categoryTasks[0];
+function ContentCategory({
+  groupTasks,
+  setGroupTasks,
+  updateGroups,
+  saveData,
+  categoryTasks,
+  archiveTask,
+  updateTask,
+  tasks,
+}) {
+  const [categoryTask, setCategoryTask] = useState(categoryTasks[0]);
+  
   return (
-    <div className="content-main" id={`category_${formatString(categoryTask.category)}`}>
-      <img
-        src={ellipse}
-        alt="ellipse checkbox"
-        className="ellipse"
-      />
+    <div
+      className="content-main"
+      id={`category_${formatString(categoryTask.category)}`}
+    >
+      <img src={ellipse} alt="ellipse checkbox" className="ellipse" />
       <div className="content-inner">
         <div className="content-task">
           <h3 className="activity">{categoryTask.category}</h3>
-    
-          <i class="fa-solid fa-plus add-task" />
 
+          <i className="fa-solid fa-plus add-task" />
         </div>
         {categoryTasks.map((task, index) => (
-          <ContentTask key={index} task={task} archiveTask={archiveTask} updateTask={updateTask} tasks={tasks} />
+          <ContentTask
+            saveData={saveData}
+            key={index}
+            task={task}
+            archiveTask={archiveTask}
+            updateTask={updateTask}
+            tasks={tasks}
+            updateGroups={updateGroups}
+          />
         ))}
       </div>
     </div>
