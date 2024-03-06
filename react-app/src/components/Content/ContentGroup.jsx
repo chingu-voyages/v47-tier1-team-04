af9705b3096effadb42ec5dd3cc200688b6e3cfb
Mikey Nichols <mikenichols410@gmail.com>
2024-03-05 19:09:53 -0500

    Refactor ContentGroup component to conditionally render based on archived tasks

--- react-app/src/components/Content/ContentGroup.jsx
+++ react-app/src/components/Content/ContentGroup.jsx
@@ -3,48 +3,48 @@ import formatString from "../../utils/formatString";
 import ContentCategory from "./ContentCategory";
 function ContentGroup({
   saveData,
-  setGroups,
   group,
   archiveTask,
   tasks,
   updateTask,
-  updateGroups
+  updateGroups,
 }) {
   const [groupTasks, setGroupTasks] = useState(
     tasks.filter((task) => task.group === group)
   );
-  const [categories, setCategories] = useState([...new Set(groupTasks.map((task) => task.category))])
-   const groupStr = groupTasks[0].group;
-  useEffect(() => {
-    updateGroups();
-    setGroups([...new Set(tasks.map((task) => task.group))])
-  }, [groupTasks]);
-  return (
-    <div className="content-activity" id={`group_${formatString(groupStr)}`}>
-      <h2 className="group-name">
-        {groupStr} <i className="fa-solid"></i>{" "}
-        <i className="fa fa-solid fa-edit"></i>
-      </h2>
-      {categories.map((category, index) => {
-        const categoryTasks = groupTasks.filter(
-          (task) => task.category === category
-        );
-        return (
-          <ContentCategory
-          updateGroups={updateGroups}
-            saveData={saveData}
-            key={index}
-            groupTasks={groupTasks}
-            setGroupTasks={setGroupTasks}
-            setGroups={setGroups}
-            categoryTasks={categoryTasks}
-            archiveTask={archiveTask}
-            updateTask={updateTask}
-            tasks={tasks}
-          />
-        );
-      })}
-    </div>
-  );
+  const [categories, setCategories] = useState([
+    ...new Set(groupTasks.map((task) => task.category)),
+  ]);
+  const groupStr = groupTasks[0].group;
+  
+  if (!groupTasks.every((task) => task.archived))
+    return (
+      <div className="content-activity" id={`group_${formatString(groupStr)}`}>
+        <h2 className="group-name">
+          {groupStr} <i className="fa-solid"></i>{" "}
+          <i className="fa fa-solid fa-edit"></i>
+        </h2>
+        {categories.map((category, index) => {
+          const categoryTasks = groupTasks.filter(
+            (task) => task.category === category
+          );
+          return (
+            <ContentCategory
+              group={group}
+              category={category}
+              updateGroups={updateGroups}
+              saveData={saveData}
+              key={index}
+              groupTasks={groupTasks}
+              setGroupTasks={setGroupTasks}
+              categoryTasks={categoryTasks}
+              archiveTask={archiveTask}
+              updateTask={updateTask}
+              tasks={tasks}
+            />
+          );
+        })}
+      </div>
+    );
 }
 export default ContentGroup;
