9e35330520455ee3a345c8925a78a2144b6d2d32
Mikey Nichols <mikenichols410@gmail.com>
2024-03-05 19:09:37 -0500

    Refactor Content component to improve performance

--- react-app/src/components/Content/Content.jsx
+++ react-app/src/components/Content/Content.jsx
@@ -1,26 +1,18 @@
 import { useState, useEffect } from "react";
 import ContentGroup from "./ContentGroup";
-function Content({ saveData, tasks, archiveTask, updateTask, updateGroups }) {
+function Content({ tasks, archiveTask, updateTask, saveData }) {
   const [groups, setGroups] = useState([...new Set(tasks.map((task) => task.group))]);
-  useEffect(() => {
-    setGroups([...new Set(tasks.map((task) => task.group))]);
-  }, [tasks]);
+  useEffect(() => setGroups([...new Set(tasks.map((task) => task.group))]), [tasks]);
   return (
     <div id="contentAnchor" className="content">
       {groups.map((group, index) => {
-        const groupTasks = tasks.filter((task) => task.group === group);
-        
         return (
           <ContentGroup
             key={index}
-            groups={groups}
-            updateGroups={updateGroups}
             group={group}
-            groupTasks={groupTasks}
-            archiveTask={archiveTask}
             tasks={tasks}
             updateTask={updateTask}
-            setGroups={setGroups}
+            archiveTask={archiveTask}
             saveData={saveData}
           />
         );
